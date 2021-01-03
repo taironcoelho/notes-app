b5896c5833fa4a7318ddbf067753262481b6a360
Tairon Coelho <tairon.neitzel@g.globo>
2021-01-03 11:24:31 -0300

    Create NoteList component

---
+++ src/components/NotesList/NotesList.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import NoteCard from '../NoteCard/NoteCard';
+import './style.scss';
+
+export default class NotesList extends Component {
+  render() {
+    return (
+      <ul className="notes-list">
+        {this.props.notes.map((note, index) => {
+          return (
+            <li className="notes-list__item" key={index}>
+              <NoteCard note={note} />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
