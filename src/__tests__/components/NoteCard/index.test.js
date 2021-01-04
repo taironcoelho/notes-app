d8f7ec29da3e792c9cff66f2d3a6a96fcf7ac665
Tairon Coelho <tairon.neitzel@g.globo>
2021-01-03 21:22:02 -0300

    Add delete option to NoteCard

--- src/__tests__/components/NoteCard/index.test.js
+++ src/__tests__/components/NoteCard/index.test.js
@@ -3,11 +3,27 @@ import {render} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Components - NoteCard', () => {
+  const note = {
+    title: 'Note 1',
+    text: 'Description of Note 1',
+  };
+
   test('Should render correctly when it receive a note', () => {
-    const note = {title: 'Note 1', text: 'Description of Note 1'};
-    const {queryByText} = render(<NoteCard note={note} />);
+    const {queryByText, queryByTestId} = render(<NoteCard note={note} />);
 
     expect(queryByText('Note 1')).not.toBeNull();
     expect(queryByText('Description of Note 1')).not.toBeNull();
+    expect(queryByTestId('delete-icon')).not.toBeNull();
+  });
+
+  test('Call delete function on delete icon click', () => {
+    const deleteNote = jest.fn();
+    const {queryByTestId} = render(
+      <NoteCard note={note} deleteNote={deleteNote} />,
+    );
+    const deleteIcon = queryByTestId('delete-icon');
+
+    deleteIcon.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+    expect(deleteNote).toHaveBeenCalledTimes(1);
   });
 });
--- src/components/NoteCard/NoteCard.jsx
+++ src/components/NoteCard/NoteCard.jsx
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 import {ReactComponent as DeleteIconSVG} from '../../assets/images/delete-icon.svg';
 import './style.scss';
 class NoteCard extends Component {
+  constructor(props) {
+    super(props);
+    this._deleteNote = this._deleteNote.bind(this);
+  }
+  _deleteNote() {
+    this.props.deleteNote(this.props.index);
+  }
+
   render() {
     return (
       <section className="note-card">
